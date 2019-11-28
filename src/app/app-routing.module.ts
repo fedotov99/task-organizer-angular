a081a1b2aeb5943f89738e4634a3b63192e6d200
Vlad Fedotov <fedotovvlad@list.ru>
2019-11-28 12:16:54 +0300

    routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {SubordinateTasksComponent} from "./subordinate-tasks/subordinate-tasks.component";
+import {ManagerTasksComponent} from "./manager-tasks/manager-tasks.component";
+import {RegistrationComponent} from "./registration/registration.component";
+import {LoginComponent} from "./login/login.component";
+import {LogoutComponent} from "./logout/logout.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: RegistrationComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'logout', component: LogoutComponent },
+  { path: 'subordinate', component: SubordinateTasksComponent},
+  { path: 'manager', component: ManagerTasksComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
