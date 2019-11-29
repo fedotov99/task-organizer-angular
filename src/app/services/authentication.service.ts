7fcf416efef2590de3a187e152d5023ff32e894b
Vlad Fedotov <fedotovvlad@list.ru>
2019-11-29 12:48:16 +0300

    handling errors

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,20 +1,35 @@
 import { Injectable } from '@angular/core';
+import {Observable, of} from "rxjs";
+import {catchError, tap} from "rxjs/operators";
+import {AuthRespond} from "../classes/auth-respond";
+import {AuthBody} from "../classes/auth-body";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {MessageService} from "./message.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  private authUrl = 'http://localhost:8080/login';
 
-  constructor() { }
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
-  authenticate(email, password) {
-    if (email === "email" && password === "password") {
-      sessionStorage.setItem('sessionUserID', email)
-      sessionStorage.setItem('role', 'manager'); // TODO: set role
-      return true;
-    } else {
-      return false;
-    }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+  authenticate(email, password): Observable<AuthRespond> {
+    const url = `${this.authUrl}`;
+    let authBody: AuthBody = new AuthBody();
+    authBody.email = email;
+    authBody.password = password;
+
+    return this.http.post<AuthRespond>(url, authBody, this.httpOptions).pipe(
+      tap((newAuthRespond: AuthRespond) => this.log(`authentication respond`)),
+      catchError(this.handleError<AuthRespond>('authenticate'))
+    );
   }
 
   isUserLoggedIn() {
@@ -24,6 +39,30 @@ export class AuthenticationService {
   }
 
   logOut() {
-    sessionStorage.removeItem('email')
+    sessionStorage.removeItem('sessionUserID')
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  private log(message: string) {
+    this.messageService.add(`ManagerService: ${message}`);
   }
 }
