2507c10be34df64179732d4b18005920063d2186
Vlad Fedotov <fedotovvlad@list.ru>
2019-11-29 12:47:38 +0300

    accessory auth classes added

---
+++ src/app/classes/auth-body.spec.ts
@@ -0,0 +1,7 @@
+import { AuthBody } from './auth-body';
+
+describe('AuthBody', () => {
+  it('should create an instance', () => {
+    expect(new AuthBody()).toBeTruthy();
+  });
+});
---
+++ src/app/classes/auth-respond.spec.ts
@@ -0,0 +1,7 @@
+import { AuthRespond } from './auth-respond';
+
+describe('AuthRespond', () => {
+  it('should create an instance', () => {
+    expect(new AuthRespond()).toBeTruthy();
+  });
+});
